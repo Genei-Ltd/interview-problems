affede3bdde73c092d952a16075e9303215c29d1
Graham Mendick <graham.mendick@gmail.com>
2020-11-17 15:56:47 +0000

    Located slice start and end from json key

--- recon/src/components/JsonToReact.js
+++ recon/src/components/JsonToReact.js
@@ -1,17 +1,21 @@
 function JsonToReact({ recon, nlp }) {
   return (
     <>
-      <div dangerouslySetInnerHTML={{__html: getHTML(recon)}} />
-      <div>{getHTML(recon)}</div>
+      <div dangerouslySetInnerHTML={{__html: getHTML(recon, nlp)}} />
+      <div>{getHTML(recon, nlp)}</div>
     </>
   )
 }
 
-function getHTML(el) {
-  const {tag, val = '', attrs: {src} = {}, children = []} = el;
+function getHTML(el, nlp) {
+  const {reconKeyToSlice} = nlp;
+  const {key, tag, val = '', attrs: {src} = {}, children = []} = el;
+  if (val) {
+    const {span: {start, end}} = reconKeyToSlice.find(({uuid}) => uuid === key);
+  }
   let html = `<${tag}${src ? ' src=' + src : ''}>${val}`;
   children.map((child, i) => {
-    html += getHTML(child)
+    html += getHTML(child, nlp)
   });
   html += `</${tag}>`;
   return html;
